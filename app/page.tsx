9a4a1b1b910df75d7c8258ba9fe85b7f9e8b6d89
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-08-20 19:31:12 +0000

    hero section done

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import { UserButton } from "@clerk/nextjs";
-import Image from "next/image";
+"use client";
+
+import Hero from "@/components/Home/Hero";
 
 export default function Home() {
   return (
-    <div>
-      <h2>Subscribe to channel</h2>
-    
+    <div className="p-5 sm:px-10 md:px-20">
+      <Hero />
     </div>
   );
 }
