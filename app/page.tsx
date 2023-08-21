a285140349768d695d800410ae9a3fb7dea2b0ad
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-08-21 08:45:09 +0000

    Graphql api request completed

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,23 @@
 import CarsFilterOptions from "@/components/Home/CarsFilterOptions";
 import Hero from "@/components/Home/Hero";
 import SearchInput from "@/components/Home/SearchInput";
+import { getCarsList } from "@/services";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    getCarList_();
+  }, []);
+
+  const getCarList_ = async () => {
+    const result = await getCarsList();
+    console.log(result);
+  };
   return (
     <div className="p-5 sm:px-10 md:px-20">
       <Hero />
-      <SearchInput/>
-      <CarsFilterOptions/>
+      <SearchInput />
+      <CarsFilterOptions />
     </div>
   );
 }
---
+++ services/index.tsx
@@ -0,0 +1,23 @@
+import request, { gql } from "graphql-request";
+
+export const getCarsList = async () => {
+  const query = gql`
+    query CarLists {
+      carLists {
+        carAvg
+        createdAt
+        id
+        name
+        price
+        publishedAt
+        updatedAt
+      }
+    }
+  `;
+
+  const result = await request(
+    "https://api-us-east-1-shared-usea1-02.hygraph.com/v2/cllkldth100i201t7djpo0ja7/master",
+    query
+  );
+  return result;
+};
